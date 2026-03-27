836f3d78d7b4b09139f93cdacefa54c8478144d5
julioinrainbows <juliocesarsouaz@gmail.com>
2026-03-26 21:20:34 -0300

    Add files via upload

---
+++ src/main/java/com/dupas/fy/Single.java
@@ -0,0 +1,38 @@
+package com.dupas.fy;
+import com.dupas.fy.TrackContainer;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Single implements TrackContainer {
+    private String singleName;
+    private List<String> artists;
+    private List<String> tags;
+    private LocalDate dateOfRelease;
+
+    @Override
+    public List<Track> getTracks() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return "";
+    }
+
+    @Override
+    public List<String> getTags(){
+        return null;
+    }
+
+    @Override
+    public LocalDate getReleaseDate(){
+        return null;
+    }
+
+    @Override
+    public List<String> getArtists(){
+        return null;
+    };
+}
